11e950b0c7e1ad810baa90f4a34a7bffa96fd818
Thomas <tran.thomas.thien@gmail.com>
2026-02-09 10:45:56 +0100

    Génération des controllers et des fichiers twig

---
+++ src/Form/CompetitionType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Championnat;
+use App\Entity\Competition;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompetitionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('championnat', EntityType::class, [
+                'class' => Championnat::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Competition::class,
+        ]);
+    }
+}
